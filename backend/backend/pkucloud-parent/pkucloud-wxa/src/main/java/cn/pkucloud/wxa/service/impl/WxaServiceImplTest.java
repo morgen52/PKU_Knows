e84aa4fc1ec395469558247c0d322866704db659
Morgen·Liu <1271989008@qq.com>
2020-12-15 11:16:09 +0800

    test of wxa&msg
    test of wxa&msg

---
+++ backend/backend/pkucloud-parent/pkucloud-wxa/src/main/java/cn/pkucloud/wxa/service/impl/WxaServiceImplTest.java
@@ -0,0 +1,18 @@
+package cn.pkucloud.wxa.service.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WxaServiceImplTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getScene() {
+    }
+}

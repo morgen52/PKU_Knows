68d45875b476bc1b89f5bdec9835de0b672449a7
Morgen·Liu <1271989008@qq.com>
2020-12-31 00:12:33 +0800

    delete

--- backend/pkucloud-qa/src/main/java/cn/pkucloud/qa/repository/QuestionRepository.java
+++
@@ -1,7 +0,0 @@
-package cn.pkucloud.qa.repository;
-
-import cn.pkucloud.qa.entity.Question;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface QuestionRepository extends PagingAndSortingRepository<Question, String> {
-}
